133989830129ec048ed97d2af55229797ac1e3a8
Duy Nguyen <duy.nguyen@akqa.com>
2017-09-06 14:42:18 +1000

    v1: basic converter.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  prName: string;
+  convertedBranchName: string;
+
+  doConvert() {
+    const prNameParams = this.prName.split('\n');
+    const ticketId = prNameParams[0];
+    delete prNameParams[0];
+    const ticketName = prNameParams.join(' ').trim().toLowerCase()
+      .replace(/\n/g, '-')
+      .replace(/ /g, '-')
+      .replace(/\./g, '');
+    this.convertedBranchName = `${ticketId}-${ticketName}`;
+  }
 }
